17423b35efd2cbfc277f7b64d87be7b1cee274b1
sholabioku <bilushonline@gmail.com>
2022-06-12 14:59:27 +0100

    Add GET /api/products/slug/:slug route and functionality

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,16 @@ app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
 
+app.get('/api/products/slug/:slug', (req, res) => {
+  const { slug } = req.params;
+  const product = data.products.find((p) => p.slug === slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send('Product not found');
+  }
+});
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
