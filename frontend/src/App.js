9682ee3dad82f91b4b48990549e5992ea6caeb54
sholabioku <bilushonline@gmail.com>
2022-06-13 07:49:33 +0100

    Add functionality to add cartItem to cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import { Container, Navbar } from 'react-bootstrap';
+import { Badge, Container, Nav, Navbar } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import { Store } from './Store';
+import { useContext } from 'react';
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
+
   return (
     <BrowserRouter>
       <div className='d-flex flex-column site-container'>
@@ -15,6 +20,16 @@ function App() {
               <LinkContainer to='/'>
                 <Navbar.Brand>amazona</Navbar.Brand>
               </LinkContainer>
+              <Nav className='me-auto'>
+                <Link to='/cart' className='nav-link'>
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg='danger'>
+                      {cart.cartItems.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
             </Container>
           </Navbar>
         </header>
