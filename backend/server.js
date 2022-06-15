bbda6a20350ac31deaab615a4cfd7f628f2db826
sholabioku <bilushonline@gmail.com>
2022-06-15 09:57:02 +0100

    Add POST /api/users/signin route and its functionality

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import colors from 'colors';
 
-import data from './data.js';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -23,8 +23,12 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
