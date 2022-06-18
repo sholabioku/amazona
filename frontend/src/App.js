e03b62ab7b1e3eb09c7df48b2af1a5cec86f1055
sholabioku <bilushonline@gmail.com>
2022-06-18 16:58:09 +0100

    A fix and add OrderScreen and its functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -92,7 +92,7 @@ function App() {
               <Route path='/shipping' element={<ShippingAddressScreen />} />
               <Route path='/payment' element={<PaymentMethodScreen />} />
               <Route path='/placeorder' element={<PlaceOrderScreen />} />
-              <Route path='/order/:id' element={<OrderScreen />} />
+              <Route path='/orders/:id' element={<OrderScreen />} />
               <Route path='/' element={<HomeScreen />} />
             </Routes>
           </Container>
