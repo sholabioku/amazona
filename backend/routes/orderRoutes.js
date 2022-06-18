a947e583ee852bfb53f98f4b1db1ff5922ee5f32
sholabioku <bilushonline@gmail.com>
2022-06-18 15:02:22 +0100

    Add POST /api/orders route and its functionality

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import Order from '../models/orderModel.js';
+
+import User from '../models/userModel.js';
+import { generateToken, isAuth } from '../utils.js';
+
+const orderRouter = express.Router();
+
+orderRouter.post(
+  '/',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const newOrder = new Order({
+      orderItems: req.body.orderItems.map((item) => ({
+        ...item,
+        product: item._id,
+      })),
+      shippingAddress: req.body.shippingAddress,
+      paymentMethod: req.body.paymentMethod,
+      itemsPrice: req.body.itemsPrice,
+      shippingPrice: req.body.shippingPrice,
+      taxPrice: req.body.taxPrice,
+      totalPrice: req.body.totalPrice,
+      user: req.user._id,
+    });
+
+    const order = await newOrder.save();
+    res.status(201).send({ message: 'New order created', order });
+  })
+);
+
+export default orderRouter;
