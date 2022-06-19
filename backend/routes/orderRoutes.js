f6b4bda7f680a819efeab74c8f60af450817d4f9
sholabioku <bilushonline@gmail.com>
2022-06-19 09:00:47 +0100

    Add GET /api/orders/mine API route and its functionality

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -31,6 +31,15 @@ orderRouter.post(
 );
 
 orderRouter.get(
+  '/mine',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const orders = await Order.find({ user: req.user._id });
+    res.send(orders);
+  })
+);
+
+orderRouter.get(
   '/:id',
   isAuth,
   expressAsyncHandler(async (req, res) => {
@@ -40,15 +49,6 @@ orderRouter.get(
     } else {
       res.status(404).send({ message: 'Order Not Found' });
     }
-  })
-);
-
-orderRouter.get(
-  '/mine',
-  isAuth,
-  expressAsyncHandler(async (req, res) => {
-    const orders = await Order.find({ user: req.user._id });
-    res.send(orders);
   })
 );
 
