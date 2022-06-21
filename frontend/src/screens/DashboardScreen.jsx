19f9f5d8c7a3805b91cedc3b5a7d232b775b1432
sholabioku <bilushonline@gmail.com>
2022-06-21 14:23:37 +0100

    Make ajax request to fetchData from GET /api/orders/summary API functionality

--- frontend/src/screens/DashboardScreen.jsx
+++ frontend/src/screens/DashboardScreen.jsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useContext, useEffect, useReducer } from 'react';
+import { Store } from '../Store';
+import { getError } from '../utils';
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -25,6 +28,30 @@ const reducer = (state, action) => {
 };
 
 const DashboardScreen = () => {
+  const [{ loading, error, summary }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: '',
+  });
+
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get('/api/orders/summary', {
+          headers: {
+            Authorization: `Bearer ${userInfo.token}`,
+          },
+        });
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+      }
+    };
+    fetchData();
+  }, [userInfo]);
+
   return <div>DashboardScreen</div>;
 };
 
