739e10d0554dbe7ed4967ca42c1930ef766f7cf2
sholabioku <bilushonline@gmail.com>
2022-06-21 14:08:41 +0100

    Add reducer for DashboardScreen and its functionality

--- frontend/src/screens/DashboardScreen.jsx
+++ frontend/src/screens/DashboardScreen.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return {
+        ...state,
+        loading: true,
+      };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        summary: action.payload,
+        loading: false,
+      };
+    case 'FETCH_FAIL':
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
+
 const DashboardScreen = () => {
   return <div>DashboardScreen</div>;
 };
