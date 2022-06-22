7c221ec41d125d2bcc95da951c34383583e4638f
sholabioku <bilushonline@gmail.com>
2022-06-22 07:44:11 +0100

    Add functionality to display info box for users, orders and totalSales

--- frontend/src/screens/DashboardScreen.jsx
+++ frontend/src/screens/DashboardScreen.jsx
@@ -1,5 +1,9 @@
 import axios from 'axios';
 import React, { useContext, useEffect, useReducer } from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
+import { Helmet } from 'react-helmet-async';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 import { Store } from '../Store';
 import { getError } from '../utils';
 
@@ -52,7 +56,60 @@ const DashboardScreen = () => {
     fetchData();
   }, [userInfo]);
 
-  return <div>DashboardScreen</div>;
+  return (
+    <div>
+      <Helmet>
+        <title>Dashboard</title>
+      </Helmet>
+      <h1>Dashboard</h1>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant='danger'>{error}</MessageBox>
+      ) : (
+        <>
+          <Row>
+            <Col md={4}>
+              <Card>
+                <Card.Body>
+                  <Card.Title>
+                    {summary.users && summary.users[0]
+                      ? summary.users[0].numUsers
+                      : 0}
+                  </Card.Title>
+                  <Card.Text>Users</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card>
+                <Card.Body>
+                  <Card.Title>
+                    {summary.orders && summary.orders[0]
+                      ? summary.orders[0].numOrders
+                      : 0}
+                  </Card.Title>
+                  <Card.Text>Orders</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card>
+                <Card.Body>
+                  <Card.Title>
+                    {summary.orders && summary.orders[0]
+                      ? summary.orders[0].totalSales.toFixed(2)
+                      : 0}
+                  </Card.Title>
+                  <Card.Text>Orders</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default DashboardScreen;
