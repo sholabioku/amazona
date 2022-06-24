b9797267a5f2d29448babf260e6cebb10846c322
sholabioku <bilushonline@gmail.com>
2022-06-24 18:08:39 +0100

    Add functionality to display edit page

--- frontend/src/screens/ProductEditScreen.jsx
+++ frontend/src/screens/ProductEditScreen.jsx
@@ -1,7 +1,11 @@
 import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react';
 import { useReducer } from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Helmet } from 'react-helmet-async';
 import { useParams } from 'react-router-dom';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 import { Store } from '../Store';
 import { getError } from '../utils';
 
@@ -68,9 +72,92 @@ const ProductEditScreen = () => {
       }
     };
     fetchData();
-  }, []);
+  }, [productId]);
 
-  return <div></div>;
+  return (
+    <Container className='small-container'>
+      <Helmet>
+        <title>Edit Product ${productId}</title>
+      </Helmet>
+      <h1>Edit Product {productId}</h1>
+
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant='danger'>{error}</MessageBox>
+      ) : (
+        <Form>
+          <Form.Group className='mb-3' controlId='name'>
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='slug'>
+            <Form.Label>Slug</Form.Label>
+            <Form.Control
+              value={slug}
+              onChange={(e) => setSlug(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='name'>
+            <Form.Label>Price</Form.Label>
+            <Form.Control
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='image'>
+            <Form.Label>Image File</Form.Label>
+            <Form.Control
+              value={image}
+              onChange={(e) => setImage(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='category'>
+            <Form.Label>Category</Form.Label>
+            <Form.Control
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='brand'>
+            <Form.Label>Brand</Form.Label>
+            <Form.Control
+              value={brand}
+              onChange={(e) => setBrand(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='countInStock'>
+            <Form.Label>Count In Stock</Form.Label>
+            <Form.Control
+              value={countInStock}
+              onChange={(e) => setCountInStock(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='description'>
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <div className='mb-3'>
+            <Button type='submit'>Update</Button>
+          </div>
+        </Form>
+      )}
+    </Container>
+  );
 };
 
 export default ProductEditScreen;
