c939acea6d7a6a1045d5a86edcc5d3f8707b0d15
sholabioku <bilushonline@gmail.com>
2022-06-25 17:41:13 +0100

    Add OrderListScreen,reducer and fetchData functionality

---
+++ frontend/src/screens/OrderListScreen.jsx
@@ -0,0 +1,60 @@
+import React, { useContext, useEffect, useReducer } from 'react';
+import axios from 'axios';
+
+import { Store } from '../Store';
+import { getError } from '../utils';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return {
+        ...state,
+        loading: true,
+      };
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        orders: action.payload,
+        loading: false,
+      };
+    case 'FETCH_FAIL':
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
+
+const OrderListScreen = () => {
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+
+  const [{ loading, error, orders }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: '',
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch({ type: 'FETCH_REQUEST' });
+        const { data } = await axios.get('/api/orders', {
+          headers: {
+            Authorization: `Bearer ${userInfo.token}`,
+          },
+        });
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
+      }
+    };
+    fetchData();
+  }, [userInfo]);
+
+  return <div>OrderListScreen</div>;
+};
+
+export default OrderListScreen;
