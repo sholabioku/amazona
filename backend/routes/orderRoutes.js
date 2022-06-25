1d5ff6dc51d86283526a057a419c07b03a08d350
sholabioku <bilushonline@gmail.com>
2022-06-25 18:00:22 +0100

    Add GET /api/orders API and its functionality

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,9 +3,19 @@ import expressAsyncHandler from 'express-async-handler';
 import Order from '../models/orderModel.js';
 import User from '../models/userModel.js';
 import Product from '../models/productModel.js';
-import { generateToken, isAdmin, isAuth } from '../utils.js';
+import { isAdmin, isAuth } from '../utils.js';
 
 const orderRouter = express.Router();
+
+orderRouter.get(
+  '/',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const orders = await Order.find().populate('user', 'name');
+    res.send(orders);
+  })
+);
 
 orderRouter.post(
   '/',
