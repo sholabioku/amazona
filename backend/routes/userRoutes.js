430d1e75373c17f3b445eab85bf9038d0b009e43
sholabioku <bilushonline@gmail.com>
2022-06-26 07:18:00 +0100

    Add GET /api/users API and its functionality

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,9 +3,19 @@ import expressAsyncHandler from 'express-async-handler';
 import bcrypt from 'bcryptjs';
 
 import User from '../models/userModel.js';
-import { generateToken, isAuth } from '../utils.js';
+import { generateToken, isAdmin, isAuth } from '../utils.js';
 
 const userRouter = express.Router();
+
+userRouter.get(
+  '/',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const users = await User.find({});
+    res.send(users);
+  })
+);
 
 userRouter.post(
   '/signin',
