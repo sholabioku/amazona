30c5308939b3d1b686333786210acd3aaee4197c
sholabioku <bilushonline@gmail.com>
2022-06-26 11:19:43 +0100

    Create edit button

--- frontend/src/screens/UserListScreen.jsx
+++ frontend/src/screens/UserListScreen.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import React, { useContext, useEffect, useReducer } from 'react';
+import { Button } from 'react-bootstrap';
 import { Helmet } from 'react-helmet-async';
+import { useNavigate } from 'react-router-dom';
 
 import LoadingBox from '../components/LoadingBox';
 import MessageBox from '../components/MessageBox';
@@ -25,6 +27,7 @@ const reducer = (state, action) => {
   }
 };
 const UserListScreen = () => {
+  const navigate = useNavigate();
   const [{ loading, error, users }, dispatch] = useReducer(reducer, {
     loading: true,
     error: '',
@@ -78,7 +81,15 @@ const UserListScreen = () => {
                 <td>{user.name}</td>
                 <td>{user.email}</td>
                 <td>{user.isAdmin ? 'YES' : 'NO'}</td>
-                <td></td>
+                <td>
+                  <Button
+                    type='button'
+                    variant='light'
+                    onClick={() => navigate(`/admin/user/${user._id}`)}
+                  >
+                    Edit
+                  </Button>
+                </td>
               </tr>
             ))}
           </tbody>
